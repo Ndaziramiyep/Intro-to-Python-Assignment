1eb24c993b4800a7857c76fbc1383615b9ceea08
Ndaziramiyep <patrickofficial30@gmail.com>
2024-09-28 16:48:20 +0200

    Adding user-input.py

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("Please enter your name: ")
+age = input("Please enter your age: ")
+location = input("Please enter your location: ")
+
+print(f"Hello, {name}! Your age is {age} years old. You live in {location}.")
